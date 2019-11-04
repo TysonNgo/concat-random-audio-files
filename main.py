bab6245a270976d7d83cd25e970d116c4a884513
T <tyson_ngo@hotmail.com>
2019-11-04 18:53:42 -0500

    implementing progress output

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from pathlib import Path
+from subprocess import Popen
 import json
 import os
 import random
+import sys
 
 
 class NotAnAudioFile(Exception):
@@ -28,10 +30,14 @@ def assertConfig():
     AssertConfig.assertNumber(config["duration_in_seconds"])
     AssertConfig.assertNumber(config["number_of_random_files"])
 
-def generate_files():
+def generate_ffmpeg_commands():
+    """
+    rtype: List(str)
+    """
     ffmpeg = f'"{config["ffmpeg_path"]}" -hide_banner -loglevel panic'
     duration = config['duration_in_seconds']
     files_to_process = config['number_of_random_files']
+    concurrent_ffmpeg_proc = config['number_of_concurrent_ffmpeg_processes']
     out_dir = config['out_dir']
     out_file_prefix = config['out_file_prefix']
     ext = config["extension"]
@@ -39,8 +45,11 @@ def generate_files():
     files = get_file_list()
     random.shuffle(files)
 
+    cmds = []
+
     for _ in range(files_to_process):
         length = 0
+        prev_length, retries = length, 0
         inputs = []
         while length < duration:
             # TODO fix
@@ -48,18 +57,27 @@ def generate_files():
             # if the input directory does not contain audio files
             # in any of its sub directories
 
+            if retries > 10:
+                print (f'Cannot create an audio file of {duration} seconds')
+                sys.exit(1)
+
             if not files:
                 files = get_file_list()
             f = files.pop()
             try:
+                prev_length = length
                 length += get_duration(f)
                 inputs.append(f)
             except NotAnAudioFile as ex:
                 print(ex)
+                continue
+            if prev_length == length:
+                retries += 1
 
         out_file = os.path.join(out_dir,  f'{out_file_prefix}{_}.{ext}')
         for i in range(len(inputs)):
             inputs[i] = f'-i "{inputs[i]}"'
+
         filters = '-filter_complex ' + \
                   '"' + \
                   ''.join([f'[{i}:a]' for i in range(len(inputs))]) + \
@@ -73,27 +91,34 @@ def generate_files():
         # -map [out] -t 600 -y ".\out0.mp3"
         ffmpeg_cmd = f'{ffmpeg} {" ".join(inputs)} {filters} -map [out] -t {duration} -y "{out_file}"'
 
-        # TODO
-        # output when ffmpeg subprocesses finish
-
         print (ffmpeg_cmd)
-        os.popen(ffmpeg_cmd)
+        cmds.append(ffmpeg_cmd)
+        #os.popen(ffmpeg_cmd)
+
+    return cmds
+
+def spawn_ffmpeg_processes(commands):
+    print(len(commands))
+    Popen(commands[0].split(' '), stdout=sys.stdout, stdin=sys.stdin)
 
 def get_file_list():
-    return list(Path(config['in_dir']).glob('**/*'))
+    files = list(Path(config['in_dir']).glob('**/*'))
+    return files
 
 def get_duration(file):
     ffprobe = f'"{config["ffprobe_path"]}"'
     file = f'"{file}"'
     ffprobe_cmd = f'{ffprobe} -v error -show_entries format=duration -of default=noprint_wrappers=1:nokey=1 -i {file}'
     result = os.popen(ffprobe_cmd).read()
-    if not result:
+    try:
+        return float(result)
+    except:
         raise NotAnAudioFile(file)
-    return float(result)
 
 with open('config.json') as f:
     config = json.load(f)
     assertConfig()
 
 if __name__ == '__main__':
-    generate_files()
+    cmds = generate_ffmpeg_commands()
+    spawn_ffmpeg_processes(cmds)
